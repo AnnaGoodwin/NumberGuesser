5456d951651eca3468cf3d40cea0e3bf9f19d38f
Jacob Ogren <jogren0044@gmail.com>
2019-05-08 16:27:58 -0600

    Add challenger name input and output functionality

--- main.js
+++ main.js
@@ -9,3 +9,15 @@ updateButton.addEventListener('click', function () {
   minRangeField.innerText = minRangeInput.value;
   maxRangeField.innerText = maxRangeInput.value;
 });
+
+var submitButton = document.querySelector('.submit-button');
+var challenger1NameInput = document.getElementById('challenger1-name-input');
+var challenger2NameInput = document.getElementById('challenger2-name-input');
+var challenger1NameOutput = document.getElementById('challenger1-name-output');
+var challenger2NameOutput = document.getElementById('challenger2-name-output');
+
+submitButton.addEventListener('click', function () {
+  event.preventDefault();
+  challenger1NameOutput.innerText = challenger1NameInput.value;
+  challenger2NameOutput.innerText = challenger2NameInput.value;
+})
